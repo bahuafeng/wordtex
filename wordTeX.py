6c5375b94a0fc9770e34f4231eee155d50bbbb85
Garrett Berg <Garrett@cloudformdesign.com>
2013-10-06 02:06:26 -0500

    Again, going in a bit of a new direction. Nothing has actually been tested yet...

--- wordTeX.py
+++ wordTeX.py
@@ -37,9 +37,23 @@ Macros = {"\\to":"\\rightarrow",
           "\\S":"&sect;"
          }
 
-begin = {'document': delete_outside,
-         'lstlisting': None,
-         }
+
+#TODO: Need begins for colors and tables
+
+# These are in the format (function_call, start_str, end_str)
+
+class tex_part(object):
+    r'''this class is a text object, normally preceeded by either a begin
+    statement or a single line (i.e. \header) and ended by an end statement
+    or a \n
+    '''
+    def __init__(self, start, text_block, end):
+        '''Should be used with get_objects_inout. start has value == 2,
+        the text block is all values inbetween == 1 and end has value == 3
+        '''
+        self.start = start
+        self.text_block = text_block
+        self.end = end
 
 #from functools import update_wrapper
 #def dec_interpret_matches(function):
@@ -58,36 +72,48 @@ def call_items(array, call):
     '''perform a functoin call on every item in an array'''
     return [call(n) for n in ltxt]
 
-def delete_outside(text, matches, call = None):
+# These two functions remain simple because they are simple -- they
+# delete everything on the inside / outside of the matches.
+# They should only be used at the beginning to simplify the stuff that
+# is absolutely not needed.
+def delete_outside(text, matches):
     '''only keep text that is inside and call a function on it
-    matches = inside_list, start_list, end_list. See get_text_inout for
+    matches = inside_list, start_list, end_list. See get_objects_inout for
     documentation'''
-    isin = get_text_inout(text, *matches)
+    isin = get_objects_inout(text, *matches)
     ltxt = [n[1] for n in inout if n[0]]
-    if call:
-        ltxt = call_item(ltxt)
     return ''.join(ltxt)
 
-def delete_inside(text, matches, call = None):
+def delete_inside(text, matches):
     '''only keep text that is outside and call a function on it
-    matches = inside_list, start_list, end_list. See get_text_inout for
+    matches = inside_list, start_list, end_list. See get_objects_inout for
     documentation'''
-    isin = get_text_inout(text, *matches)
+    isin = get_objects_inout(text, *matches)
     ltxt = [n[1] for n in inout if not n[1]]
-    if call:
-        ltxt = call_items(ltxt)
     return ''.join(ltxt)
 
 def handle_inside(text, matches, call = None, 
-                  do_inlist = None, do_stlist = None, do_endlist = None)
+                  do_inlist = None, do_stlist = None, do_endlist = None):
     '''Handles the data inside the start/end by executing call on it. Does
     not remove it from the text.
-    matches = inside_list, start_list, end_list. See get_text_inout for
+
+    matches = inside_list, start_list, end_list. See get_objects_inout for
     documentation
-    '''
+    
     do_inlist: execute this function on the matches for 'inside_list'
     do_stlist: execute this function on the matches for 'start_list'
     do_endlist: execute this function on the matches for 'end_list'
+    simple example:
+        do_endlist = [[r'\\fi', lambda inout_val, in_str: 'this was an end' + in_str]]
+        # replaces all end fi's with 'this was an end: \fi'
+    So each call recieves the inout_val (0, 1, 2, or 3) and it's own text
+    (Note: text is interpreted as a regular expression)
+    '''
+    #TODO: Going to wait till I actually need to use this to flush it out
+    isin = get_objects_inout(text, *matches)
+    if do_inlist:
+        pass
+        
 
 ###################################3
 ## GENERAL FUNCTIONS   
@@ -97,13 +123,17 @@ def split_text(matches, text):
     matches = re.compile('|'.join(matches))
     return matches, matches.split(text)
 
-def get_text_inout(raw_text, inside_list, starters_list, end_list):
+def get_objects_inout(raw_text, inside_list, starters_list, end_list):
     '''given the matches, returns the text in order in tuples as the following
+    (2, txt3),  # value was the first inside start of group    
     (True, txt1),
+    (True, txt2),
+    (3, txt4),  # text was the final end of group
     (False, txt2),
     etc...
+    
     where True means that the text is inside your match parameters and False
-    means they are outside
+    means they are outside. 2 and 3 are documented above.
     
     Note, the inside list takes precedence over the starter list, and the starter
     list takes precedence over the end list.
@@ -126,13 +156,17 @@ def get_text_inout(raw_text, inside_list, starters_list, end_list):
     end = [re.compile(m) for m in end_list]
     
     num_in = 0
+    set_num = None
     ltext = []
     #TODO: It has to match arbitrary if statements. I think this should be
     # pretty easy
     for txt in splited:
+        assert(num_in >= 0)
         if txt in (None, ''):
             continue
         if re_in(txt, inside):
+            if num_in == 0:
+                set_num = 2
             num_in += 1
         elif inside > 0 and re_in(txt, starter):
             # i.e. if you wrote something like /iffalse /ifblog
@@ -140,7 +174,12 @@ def get_text_inout(raw_text, inside_list, starters_list, end_list):
         elif re_in(txt, end):
             # make sure we only count ends if you are removing!
             num_in -= 1
-        if num_in == 0:
+            if num_in == 0:
+                set_num = 3
+        if set_num:
+            ltext.append((set_num, txt))
+            set_num = None
+        elif num_in > 0:
             ltext.append((True, txt))
         else:
             ltext.append((False, txt))
@@ -171,15 +210,13 @@ def check_brackets(match_list, end_list, text):
         raise LaTeX_Error("Brackets not matched")
     return match_cmp, found, gnumbers
     
-class LatexWp(object):
+class LatexWordpress(object):
+    '''Reads a LaTeX text document and processes it.
+    The class works by first filtering out stuff that is absolutely not needed
+    (i.e. comments) and then splitting up the document into various class
+    handlers. These are defined in #TODO
+    '''
     def __init__(self, path):
         with open(path) as f:
             self.text = f.read()
         self.get_doc()
-    
-    def get_doc(self):
-        ''' I have to decide the general overview of how these functions work.
-        I think I will be constructing the re like:
-            ({0})|({1})|... This will let me pull out all the data.
-        '''
-        pass
