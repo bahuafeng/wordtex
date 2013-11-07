f6955fbdef4d90a1b622cb58cc4b6f4a66a2ca91
Garrett Berg <Garrett@cloudformdesign.com>
2013-11-07 14:33:36 -0800

    One version of setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,24 @@
 
 from distutils.core import setup
 import publish
-
-setup(name='wordtex',
-      version=publish.VERSION,
-      description='Latex to Word Press HTML converter',
-      author='Garrett Berg',
-      author_email='garrett@cloudformdesign.com',
-      url='https://github.com/cloudformdesign/wordtex',
-      packages = ['wordtex']
-#      packages=['wordtex', 'wordtex.cloudtb'],
-#      package_dir = {'wordtex.cloudtb': '_publish'}
-     )
+import shutil
+shutil.move(publish.publish.CLOUDTB_PATH, 
+            publish.publish.CLOUDTB_PACKAGE_STR)
+try:
+    setup(name='wordtex',
+          version=publish.VERSION,
+          description='Latex to Word Press HTML converter',
+          author='Garrett Berg',
+          author_email='garrett@cloudformdesign.com',
+          url='https://github.com/cloudformdesign/wordtex',
+#          modules = ['wordtex', 'wp_formatting', 'texlib'],
+          packages = ['wordtex', 'wordtex.cloudtb',],
+          package_dir = {'wordtex': ''}
+    #      packages=['wordtex', 'wordtex.cloudtb'],
+         )
+finally:
+    import pdb
+    pdb.set_trace()
+    shutil.move(publish.publish.CLOUDTB_PACKAGE_STR, 
+                publish.publish.CLOUDTB_PATH)
+    
