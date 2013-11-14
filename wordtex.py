ff2dab0a959a94dfa2ef4741e2d3b362228b1baa
Garrett Berg <Garrett@cloudformdesign.com>
2013-11-14 14:11:03 -0700

    After banging my head against the wall for a while, I still can't get lyx to even execute a python command. Needs more work.

--- wordtex.py
+++ wordtex.py
@@ -19,38 +19,57 @@
 #     License along with wordtex.  If you can't find it,
 #     see <http://www.gnu.org/licenses/>
 
+import pdb
 import sys, os
 sys.path.insert(1, '..')
-from cloudtb import dbe, system
+from cloudtb import dbe
+from cloudtb import system, logtools
+log = logtools.get_logger()
+logtools.log_fatal_exception()
+log.info('starting')
 
 import texlib
 
 document = None
 
 def main():
+    import argparse
     import wp_formatting
+    import publish
     import pdb
     
     texlib.TexPart.FORMAT_MODULE = wp_formatting
-    argv = sys.argv
-    inputfile = "tex_docs/simple.tex"
-    inputfile = "/home/user/Documents/Cloudform Design/Website/Blog/Projects In the works.tex"
-    inputfile = "/home/user/Documents/Cloudform Design/Website/Personal/Resume.tex"
 
-    if len(argv) > 1:
-        inputfile = argv[1]
-    else:
-        inputfile = os.path.join(os.getcwd(), inputfile)
-
-    if len(argv) > 2:
-        outputfile = argv[2]
-    else:
-        if system.is_file_ext(inputfile, 'tex'):
-            outputfile = inputfile[:-4] + ".wp.html"
+    parser = argparse.ArgumentParser(description = "Convert .tex file to "
+        "wordpress html.")
+    parser.add_argument('file_input',
+                        help = "The input path")
+    parser.add_argument('file_output',
+                        default = '', nargs = '?',
+                        help = 'The output path. Default is <in>.wp.html')
+    parser.add_argument('-L', '--lyx', nargs = 2,
+                        help = "Custom option for Lyx")
+    parser.add_argument('--version', action='version', 
+                        version='%(prog)s {0}'.format(publish.VERSION))
+    
+    args = parser.parse_args()
+    
+    if args.file_output == '':
+        if system.is_file_ext(args.file_input, 'tex'):
+            args.file_output = args.file_input[:-4] + ".wp.html"
         else:
-            outputfile = inputfile + ".wp.html"
-            
-    document = texlib.process_document(inputfile)
+            args.file_output = args.file_input + ".wp.html"
+'''python -tt /home/user/Projects/CloudformDesign/PythonCloudform/wordtex/\
+wordtex.py $$i $$o --lyx $$p $$r'''
+    if args.lyx:
+        # output file needs directory
+        # lyx variable has original input file directory
+        args.file_input = args.lyx[0] + args.file_input
+        args.file_output = args.lyx[1] + args.file_output
+    
+    print "Input:", args.file_input
+    document = texlib.process_document(args.file_input)
+    
 #    texlib.print_tex_tree(document)
 #    document.check_no_update_text()
     document.format()
@@ -59,9 +78,9 @@ def main():
         html_text = bs4.BeautifulSoup(document.get_wp_text()).prettify()
     except ImportError:
         html_text = document.get_wp_text()
-    with open(outputfile, 'w') as f:
+    with open(args.file_output, 'w') as f:
         f.write(html_text)
-    print 'Done, file at :', outputfile
+    print 'Done, file at :', args.file_output
     
 if __name__ == '__main__':
     main()
